0e48d5ba09c7979d8cbeaed3636a311fc412868e
Larry Smith Jr <mrlesmithjr@gmail.coml>
2016-01-24 15:43:15 -0500

    Added mysql role
    Signed-off-by: Larry Smith Jr <mrlesmithjr@gmail.coml>

--- provision.sh
+++ provision.sh
@@ -3,5 +3,5 @@ sudo apt-get update
 sudo apt-get install -y git python-pip python-dev
 sudo pip install jinja2
 sudo pip install ansible
-sudo git clone https://github.com/mrlesmithjr/ansible-snort.git /etc/ansible/roles/ansible-snort
+sudo ansible-galaxy install -r /vagrant/requirements.yml -f
 ansible-playbook -i "localhost," -c local /vagrant/playbook.yml
